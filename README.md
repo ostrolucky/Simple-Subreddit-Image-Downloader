85c9d51feed3c60afb35f9165c4eda33347893f2
Gabriel Potkány <gadelat@gmail.com>
2014-06-07 12:06:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+Simple Subreddit Image Downloader
+==========================
+*Simple Subreddit Image Downloader* is bash script which:
+- has minimal external dependencies
+- downloads full-size images from subreddits
+- is crossplatform (tested on windows with cygwin)
+
+This script just downloads all directly linked images in subreddit. For more complex usage, use other reddit image downloader.
+
+Requirements
+============
+bash (cygwin is OK)
+wget
+
+Usage
+=====
+`./rdit.sh <subreddit_name>
---
+++ rdit.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+#cfg
+output="down"
+useragent="Love by u/gadelat"
+
+subreddit=$1
+url="https://pay.reddit.com/r/$subreddit/.json"
+content=`wget -U "$useragent" --no-check-certificate -q -O - $url`
+mkdir -p $output
+while : ; do
+    urls=$(echo -e "$content"|grep -Po '"url":.*?[^\\]",'|cut -f 4 -d '"')
+    names=$(echo -e "$content"|grep -Po '"title":.*?[^\\]",'|cut -f 4 -d '"')
+    ids=$(echo -e "$content"|grep -Po '"id":.*?[^\\]",'|cut -f 4 -d '"')
+    a=1
+    for url in $(echo -e "$urls"); do
+        if [ -n  "`echo "$url"|egrep \".gif|.jpg\"`" ]; then
+            #echo -n "$url: "
+            name=`echo -e "$names"|sed -n "$a"p`
+            id=`echo -e "$ids"|sed -n "$a"p`
+            echo $name
+            newname="$name"_"$subreddit"_$id.${url##*.}
+            wget -U "$useragent" --no-check-certificate -nv -nc -P down -O "$output/$newname" $url
+        fi
+        a=$(($a+1))
+    done
+    after=$(echo -e "$content"|grep -Po '"after":.*?[^\\]",'|cut -f 4 -d '"'|tail -n 1)
+    if [ -z $after ]; then
+        break
+    fi
+    url="https://www.reddit.com/r/$subreddit/.json?count=200&after=$after"
+    content=`wget -U "$useragent" --no-check-certificate -q -O - $url`
+    #echo -e "$urls"
+done
