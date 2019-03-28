c462b0c15ba7c17932dc7d489b9d0870f145fdf1
Gabriel Ostrolucký <gabriel.ostrolucky@gmail.com>
2019-03-28 10:01:45 +0100

    Stop echo to interpert control characters. Fixes #4

--- download-subreddit-images.sh
+++ download-subreddit-images.sh
@@ -8,21 +8,21 @@ url="https://www.reddit.com/r/$subreddit/.json?raw_json=1"
 content=`wget -U "$useragent" -q -O - $url`
 mkdir -p $subreddit
 while : ; do
-    urls=$(echo -e "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.preview.images[0].source.url')
-    names=$(echo -e "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.title')
-    ids=$(echo -e "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.id')
+    urls=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.preview.images[0].source.url')
+    names=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.title')
+    ids=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")) | .data.id')
     a=1
     wait # prevent spawning too many processes
     for url in $urls; do
-        name=`echo -e "$names"|sed -n "$a"p`
-        id=`echo -e "$ids"|sed -n "$a"p`
-        ext=`echo -e "${url##*.}"|cut -d '?' -f 1`
+        name=`echo -n "$names"|sed -n "$a"p`
+        id=`echo -n "$ids"|sed -n "$a"p`
+        ext=`echo -n "${url##*.}"|cut -d '?' -f 1`
         newname="$name"_"$subreddit"_$id.$ext
         echo $name
         wget -U "$useragent" --no-check-certificate -nv -nc -P down -O "$subreddit/$newname" $url &>/dev/null &
         a=$(($a+1))
     done
-    after=$(echo -e "$content"| jq -r '.data.after')
+    after=$(echo -n "$content"| jq -r '.data.after')
     if [ -z $after ]; then
         break
     fi
