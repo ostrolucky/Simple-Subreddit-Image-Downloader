0046879d087554a3a73d4cb29b72e8a845a7df26
makeworld <25111343+makeworld-the-better-one@users.noreply.github.com>
2020-01-19 21:29:19 -0500

    Added sort and time range option

--- download-subreddit-images.sh
+++ download-subreddit-images.sh
@@ -5,7 +5,18 @@ useragent="Love by u/gadelat"
 timeout=60
 
 subreddit=$1
-url="https://www.reddit.com/r/$subreddit/.json?raw_json=1"
+sort=$2
+top_time=$3
+
+if [ -z $sort ]; then
+    sort="hot"
+fi
+
+if [ -z $top_time ];then
+    top_time=""
+fi
+
+url="https://www.reddit.com/r/$subreddit/$sort/.json?raw_json=1&t=$top_time"
 content=`wget -T $timeout -U "$useragent" -q -O - $url`
 mkdir -p $subreddit
 while : ; do
@@ -23,11 +34,10 @@ while : ; do
         wget -T $timeout -U "$useragent" --no-check-certificate -nv -nc -P down -O "$subreddit/$newname" $url &>/dev/null &
         a=$(($a+1))
     done
-    after=$(echo -n "$content"| jq -r '.data.after')
+    after=$(echo -n "$content"| jq -r '.data.after//empty')
     if [ -z $after ]; then
         break
     fi
-    url="https://www.reddit.com/r/$subreddit/.json?count=200&after=$after&raw_json=1"
+    url="https://www.reddit.com/r/$subreddit/$sort/.json?count=200&after=$after&raw_json=1&t=$top_time"
     content=`wget -T $timeout -U "$useragent" --no-check-certificate -q -O - $url`
-    #echo -e "$urls"
 done
