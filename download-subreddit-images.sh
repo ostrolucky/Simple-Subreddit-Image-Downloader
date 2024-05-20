7686d1f84f4f8535b0dc6020aab879c01458f96b
Michael Mendy <montana@linux.com>
2024-05-20 14:50:38 -0700

    Enforced title sanitization more effectively: Using `tr -d '/\r\n'` and `sed "s/\// /g"` to remove any potential newlines and slashes from titles.
    * Replaced `while : ; do` with `while true; do:` This makes the infinite loop more readable.
    
    * Enforced title sanitization more effectively: Using `tr -d '/\r\n'` and `sed "s/\// /g"` to remove any potential newlines and slashes from titles.
    
    * Added parentheses around `i` and limit comparison: This is more readable and in line with typical Bash script practices.

--- download-subreddit-images.sh
+++ download-subreddit-images.sh
@@ -1,57 +1,58 @@
 #!/bin/bash
 
-#cfg
+print_usage() {
+    echo "Usage: $0 SUBREDDIT [hot|new|rising|top|controversial] [all|year|month|week|day] [limit]"
+    echo "Examples:   $0 starterpacks new week 10"
+    echo "            $0 funny top all 50"
+}
+
 subreddit=$1
-sort=$2
+sort=${2:-hot}
 top_time=$3
-limit=$4
+limit=${4:-0}
 
-if [ "$1" == "-h" ] || [ -z $subreddit ]; then
-echo "Usage: $0 SUBREDDIT [hot|new|rising|top|controversial] [all|year|month|week|day] [limit]
-Examples:   $0 starterpacks new week 10
-            $0 funny top all 50"
-exit 0;
+if [[ "$1" == "-h" || -z "$subreddit" ]]; then
+    print_usage
+    exit 0
 fi
 
-if [ -z $sort ]; then
-    sort="hot"
-fi
+base_url="https://www.reddit.com/r/$subreddit/$sort/.json?raw_json=1"
+url="${base_url}&t=$top_time"
+content=$(curl --retry 3 --silent "$url")
 
-if [ -z $top_time ];then
-    top_time=""
-fi
-
-if [ -z $limit ]; then
-	limit=0
-fi
-
-url="https://www.reddit.com/r/$subreddit/$sort/.json?raw_json=1&t=$top_time"
-content=`curl $url`
-mkdir -p $subreddit
+mkdir -p "$subreddit"
 i=1
-while : ; do
-    urls=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")?) | .data.preview.images[0].source.url')
-    names=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")?) | .data.title')
-    ids=$(echo -n "$content"| jq -r '.data.children[]|select(.data.post_hint|test("image")?) | .data.id')
+
+while true; do
+    images=$(echo "$content" | jq -r '.data.children[] | select(.data.post_hint == "image") | .data.preview.images[0].source.url')
+    titles=$(echo "$content" | jq -r '.data.children[] | select(.data.post_hint == "image") | .data.title')
+    ids=$(echo "$content" | jq -r '.data.children[] | select(.data.post_hint == "image") | .data.id')
+
     a=1
-    wait # prevent spawning too many processes
-    for url in $urls; do
-        name=`echo -n "$names"|sed -n "$a"p`
-        id=`echo -n "$ids"|sed -n "$a"p`
-        ext=`echo -n "${url##*.}"|cut -d '?' -f 1 | sed 's/gif/png/' `
-        newname=`echo $name | sed "s/^\///;s/\// /g"`_"$subreddit"_$id.$ext
-        printf "$i/$limit : $newname\n"
-        curl --retry 3 --no-clobber --output "$subreddit/$newname" $url &>/dev/null &
-        ((a=a+1))
-        ((i=i+1))
-        if [ $i -gt $limit ] ; then
-          exit 0
+    for url in $images; do
+        title=$(echo "$titles" | sed -n "${a}p")
+        id=$(echo "$ids" | sed -n "${a}p")
+        ext=$(echo "${url##*.}" | cut -d '?' -f 1 | sed 's/gif/png/')
+        newname=$(echo "$title" | tr -d '/\r\n' | sed "s/\// /g")_"$subreddit"_$id.$ext
+        printf "%d/%d : %s\n" "$i" "$limit" "$newname"
+        curl --retry 3 --silent --no-clobber --output "$subreddit/$newname" "$url" &
+
+        ((a++))
+        ((i++))
+
+        if (( i > limit )); then
+            wait
+            exit 0
         fi
     done
-    after=$(echo -n "$content"| jq -r '.data.after//empty')
-    if [ -z $after ]; then
+
+    after=$(echo "$content" | jq -r '.data.after // empty')
+    if [[ -z "$after" ]]; then
         break
     fi
-    url="https://www.reddit.com/r/$subreddit/$sort/.json?count=200&after=$after&raw_json=1&t=$top_time"
-    content=`curl --retry 3 $url`
+
+    url="${base_url}&count=200&after=$after&t=$top_time"
+    content=$(curl --retry 3 --silent "$url")
 done
+
+wait
